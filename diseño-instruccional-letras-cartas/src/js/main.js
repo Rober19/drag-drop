670edd9f1837b699ed0111395a091fe0c10a5f9c
rober19 <texas_telecaster@hotmail.com>
2019-04-12 14:36:31 -0500

    diseños instruccionales

---
+++ diseño-instruccional-letras-cartas/src/js/main.js
@@ -0,0 +1,44 @@
+$('#menu-toggle').click(e => {
+  e.preventDefault();
+  $('#wrapper').toggleClass('toggled');
+});
+$('#b_introduccion').click(e => {
+  console.table(0);
+  side_bar({ introduccion: true });
+});
+$('#b_objetivo').click(e => {
+  console.table(1);
+  side_bar({ objetivo: true });
+});
+$('#b_competencias').click(e => {
+  console.table(1);
+  side_bar({ competencias: true });
+});
+$('#b_evaluacion').click(e => {
+  console.table(1);
+  side_bar({ evaluacion: true });
+});
+$('#b_fuentes').click(e => {
+  console.table(1);
+  side_bar({ fuentes: true });
+});
+
+function side_bar(x) {
+  return {
+    introduccion: x.introduccion
+      ? $('#introduccion').css('display', 'block')
+      : $('#introduccion').css('display', 'none'),
+    objetivo: x.objetivo
+      ? $('#objetivo').css('display', 'block')
+      : $('#objetivo').css('display', 'none'),
+    competencias: x.competencias
+      ? $('#competencias').css('display', 'block')
+      : $('#competencias').css('display', 'none'),
+    evaluacion: x.evaluacion
+      ? $('#evaluacion').css('display', 'block')
+      : $('#evaluacion').css('display', 'none'),
+    fuentes: x.fuentes
+      ? $('#fuentes').css('display', 'block')
+      : $('#fuentes').css('display', 'none'),
+  };
+}
